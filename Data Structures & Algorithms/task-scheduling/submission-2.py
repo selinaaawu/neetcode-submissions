8a80125ec1ae80e3e37496a86ca3bb2bbb27ef55
Selina Wu <144982441+selinaaawu@users.noreply.github.com>
2026-07-17 20:43:30 -0400

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+
+        # count frequency of each task
+        freq = Counter(tasks)
+
+        # max heap for easy retrieval of most frequent task
+        # max heap stores (count, CPU tasks)
+        maxHeap = [-count for count in freq.values()]
+        heapq.heapify(maxHeap)
+
+        # queue for processing tasks, FIFO
+        # (time out of queue, -count)
+        queue = deque()
+        
+        minCycle = 0
+        while maxHeap or queue:
+            # one CPU cycle passed
+            # minCycle += 1
+
+            # if not maxHeap:
+            #     minCycle = queue[0][0]
+
+            # if most recent == current cycle, take 
+            while queue and queue[0][0] == minCycle:
+                count = queue.popleft()[1]
+                heapq.heappush(maxHeap, count)
+            
+            count = heapq.heappop(maxHeap) if maxHeap else 0
+            print(minCycle, count)
+
+            if count < -1:
+                queue.append((minCycle + n + 1, count + 1))
+            print(queue)
+
+            minCycle += 1
+
+        return minCycle
+        
