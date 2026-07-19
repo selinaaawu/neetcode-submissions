ab23b6a317b0f21fbb8a57b601763d3404ca1a1e
Selina Wu <144982441+selinaaawu@users.noreply.github.com>
2026-07-19 13:00:24 -0400

    Add: insert-new-interval - submission-3

---
+++ Data Structures & Algorithms/insert-new-interval/submission-3.py
@@ -0,0 +1,55 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
+                ]
+        res.append(newInterval)
+        return res
+        
+        # insert newInterval into intervals s.t. intervals has no overlapping intervals
+        result = []
+        i, n = 0, len(intervals)
+
+        # sort intervals by start time
+        intervals.sort(key=lambda x: x[0])
+
+        # intervals[i] completely before newInterval, no overlap
+        # intervals[i][end] < newInterval[start]
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+        print("1", result)
+        
+        # overlap & merge when intervals[i][start] <= newInterval[end]
+        # expand newInterval to cover both ranges
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+        result.append(newInterval)
+        print("2", result)
+
+        # intervals[i] completely after newInterval, no overlap
+        # newInterval[end] < intervals[i][start]
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+        print("3", result)
+        
+        print(result)
+        return result
+
+
+        
+        
